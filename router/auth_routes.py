4d4578bc7f1aa0eb8b8f33e77ca752123cc7c729
Saulo Cunha <saulorg3@gmail.com>
2025-10-25 18:31:48 -0300

    Implementado criptografica nas senhas, adicionado Status 400 para erro de email já cadastrado

--- router/auth_routes.py
+++ router/auth_routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models.models import Usuario
 from helpers.dependencies import start_session
+from main import bcrypt_context
 
 #APIRouter idica o o inicio do endpoint.
 #Aqui será configurado tudo que tera no aaaa.aaaaaa.com/pedidos
@@ -18,9 +19,10 @@ async def criar_conta(email: str, senha: str, nome: str, session = Depends(start
     usuario = session.query(Usuario).filter_by(email=email).first()
     if usuario: 
         # verifica se já existe um usuario com esse email
-        return {"mensagem": "Já existe um usuario com esse email"}
+        return HTTPException(status_code=400, detail="E-mail do usuário já cadastrado")
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_criptografada = bcrypt_context.hash(senha)
+        novo_usuario = Usuario(nome, email, senha_criptografada)
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": "Usuário cadastrado com sucesso"}
