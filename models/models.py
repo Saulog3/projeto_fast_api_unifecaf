53825e6cf47f984c5fa00916e281c50483ec3820
Saulo Cunha <saulorg3@gmail.com>
2025-10-25 12:03:14 -0300

    Criado instancia da tabela Usuario

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, ForeignKey, Float
 from sqlalchemy.orm import declarative_base
 
 #Conexão com o banco de dados
@@ -10,6 +10,21 @@ Base = declarative_base()
 # Criar as classes / tabelas do banco
 
 class Usuario(Base):
-    pass
+    __tablename__ = "usuarios"
+    id = Column("id", Integer, autoincrement=True, primary_key=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False)
+    senha = Column("senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+
+
 
 # Executa a criação dos metadados do seu banco de dados
